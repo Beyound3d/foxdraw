1d18c53eb1aec57b2c96804e1bb4c3b84e727660
Tanu Kaushik <developertanukaushik@gmail.com>
2024-02-01 12:58:05 +0530

    structure🏢

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,15 @@
 "use client"
-import React from 'react'
+
+import { UserButton } from "@clerk/nextjs"
 
 export const Navbar = () => {
   return (
-    <div className='flex items-center gap-x-4 p-5 bg-green-500'>Navbar</div>
+    <div className='flex items-center gap-x-4 p-5 bg-green-500'>
+        <div className="hidden lg:flex lg:flex-1 bg-yellow-500">
+search
+        </div>
+        <UserButton/>
+        </div>
   )
 }
 
